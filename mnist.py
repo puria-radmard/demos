57db887cf0d22492eed6f4ef10f78c55b369ffd2
P. Radmard <pr450@mirzakhani.eng.cam.ac.uk>
2023-03-31 17:01:56 +0100

    first commit

---
+++ mnist.py
@@ -0,0 +1,75 @@
+"Copied over from https://github.com/yashsmehta/helmholtz-machine/blob/master/datasets/mnist.py!"
+
+import os
+import pickle
+import gzip
+import numpy as np
+import urllib.request
+
+
+class MNIST:
+    def __init__(self, data_path, binary=False):
+        self.url = 'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
+        self.directory = data_path
+        self.filename = os.path.join(self.directory, 'mnist.pkl.gz')
+        self.binary = binary
+        self.train_rng = np.random.RandomState(283)
+
+        self._ensure_file_is_on_disk()
+        self.data = {}
+        with gzip.open(self.filename, 'rb') as f:
+            self.data['train'], self.data['valid'], self.data['test'] \
+                = pickle.load(f, encoding="latin1")
+
+    @staticmethod
+    def download(url, filename):
+        print("Downloading MNIST dataset")
+        urllib.request.urlretrieve(url, filename)
+
+    @staticmethod
+    def binarize(x, rng):
+        return (rng.rand(*x.shape) < x).astype(np.float32)
+
+    def _ensure_file_is_on_disk(self):
+        if not os.path.exists(self.directory):
+            os.mkdir(self.directory)
+        if not os.path.exists(self.filename):
+            self.download(self.url, self.filename)
+
+    def minibatch(self, subdataset, indices, rng):
+        data, labels = self.data[subdataset]
+        data, labels = data[indices], labels[indices]
+        if self.binary:
+            data = self.binarize(data, rng)
+        return data, labels
+
+    def get_random_minibatch(self, subdataset, minibatch_size, rng):
+        indices = rng.randint(self.data[subdataset][0].shape[0], size=(minibatch_size,))
+        return self.minibatch(subdataset, indices, rng)
+
+    def random_minibatches(self, subdataset, minibatch_size, num_minibatches, rng=None):
+        if subdataset != 'train' and rng is not None:
+            print('Trying to access a stream of random minibatches from valid/test set will affect training rng!')
+        if rng is None:
+            rng = self.train_rng
+        for _ in range(num_minibatches):
+            yield self.get_random_minibatch(subdataset, minibatch_size, rng)
+
+    def all_minibatches(self, subdataset, minibatch_size, rng, max_num_minibatches=None):
+        num_minibatches = 1 + (self.data[subdataset][0].shape[0] - 1) // minibatch_size
+        if max_num_minibatches is not None and max_num_minibatches < num_minibatches:
+            num_minibatches = max_num_minibatches
+
+        for i in range(num_minibatches):
+            indices = slice(i * minibatch_size, (i + 1) * minibatch_size)
+            yield self.minibatch(subdataset, indices, rng)
+
+    def reshape_for_display(self, x):
+        return x.reshape((-1, 28, 28))
+
+    def get_data_dim(self):
+        return 28*28
+
+    @property
+    def data_dim(self):
+        return self.get_data_dim()
